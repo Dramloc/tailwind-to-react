3a25e07a799249757bd69054171ac05e04999b8c
Antoine Colmard <a.colmard@gmail.com>
2020-11-30 23:56:54 +0100

    ♻ Simplify preview generation

--- src/codemods/generateImports.js
+++ src/codemods/generateImports.js
@@ -1,28 +1,33 @@
 // @ts-check
-/** @typedef {"esm" | "umd"} BundleType */
+/** @typedef {"esm" | "cdn"} BundleType */
 /** @typedef {{ type: BundleType, preset: import("./convertComponent").TailwindToReactPreset }} GenerateImportsOptions */
-/** @typedef {{ umd: string, url: string }} Dependency */
+/** @typedef {{ esm: string, cdn: string }} Dependency */
 
 const dependencies = {
   react: {
-    umd: "window.React",
-    url: "https://unpkg.com/react@17.0.1/umd/react.production.min.js",
+    esm: "react",
+    cdn: "https://cdn.skypack.dev/react?min",
   },
   "react-dom": {
-    umd: "window.ReactDOM",
-    url: "https://unpkg.com/react-dom@17.0.1/umd/react-dom.production.min.js",
+    esm: "react-dom",
+    cdn: "https://cdn.skypack.dev/react-dom?min",
   },
   "@headlessui/react": {
-    umd: "window.headlessui",
-    url: "https://unpkg.com/@headlessui/react@0.2.0/dist/headlessui.umd.production.min.js",
+    esm: "@headlessui/react",
+    cdn: "https://cdn.skypack.dev/@headlessui/react?min",
   },
   clsx: {
-    umd: "window.clsx",
-    url: "https://unpkg.com/clsx@1.1.1/dist/clsx.min.js",
+    esm: "clsx",
+    cdn: "https://cdn.skypack.dev/clsx?min",
   },
   "@emotion/react": {
-    umd: "window.emotionReact",
-    url: "https://unpkg.com/@emotion/react@11.1.1/dist/emotion-react.umd.min.js",
+    esm: "@emotion/react",
+    cdn: "https://cdn.skypack.dev/@emotion/react?min",
+  },
+  "twin.macro": {
+    esm: "twin.macro",
+    // Always generate esm imports for twin so the macro can be evaluated
+    cdn: "twin.macro",
   },
 };
 
@@ -49,59 +54,29 @@ const isNamedImportSpecifier = (importSpecifier) => !importSpecifier.isDefault;
 const printImportDeclaration = (importDeclaration, type) => {
   const defaultImport = importDeclaration.specifiers.find(isDefaultImportSpecifier);
   const namedImports = importDeclaration.specifiers.filter(isNamedImportSpecifier);
-  if (type === "esm") {
-    if (importDeclaration.specifiers.length === 0) {
-      return `import "${importDeclaration.source}";`;
-    }
+  const source = dependencies[importDeclaration.source][type];
 
-    const defaultImportString = defaultImport ? defaultImport.local : null;
-    const namedImportsString =
-      namedImports.length !== 0
-        ? "{" +
-          namedImports
-            .map((specifier) => {
-              if (specifier.local === specifier.imported) {
-                return specifier.local;
-              }
-              return `${specifier.imported} as ${specifier.local}`;
-            })
-            .join(", ") +
-          " }"
-        : null;
-
-    const imports = [defaultImportString, namedImportsString].filter(Boolean).join(", ");
-    return `import ${imports} from "${importDeclaration.source}";`;
+  if (importDeclaration.specifiers.length === 0) {
+    return `import "${source}";`;
   }
-  if (type === "umd") {
-    if (importDeclaration.specifiers.length === 0) {
-      return "";
-    }
-    const defaultImportString = defaultImport ? defaultImport.local : null;
-    const namedImportsString =
-      namedImports.length !== 0
-        ? "{" +
-          namedImports
-            .map((specifier) => {
-              if (specifier.local === specifier.imported) {
-                return specifier.local;
-              }
-              return `${specifier.imported}: ${specifier.local}`;
-            })
-            .join(", ") +
-          " }"
-        : null;
 
-    const umd = dependencies[importDeclaration.source]?.umd;
+  const defaultImportString = defaultImport ? defaultImport.local : null;
+  const namedImportsString =
+    namedImports.length !== 0
+      ? "{" +
+        namedImports
+          .map((specifier) => {
+            if (specifier.local === specifier.imported) {
+              return specifier.local;
+            }
+            return `${specifier.imported} as ${specifier.local}`;
+          })
+          .join(", ") +
+        " }"
+      : null;
 
-    if (!umd) {
-      return "";
-    }
-
-    return [defaultImportString, namedImportsString]
-      .filter(Boolean)
-      .map((importString) => `const ${importString} = ${umd};`)
-      .join("\n");
-  }
+  const imports = [defaultImportString, namedImportsString].filter(Boolean).join(", ");
+  return `import ${imports} from "${source}";`;
 };
 
 /** @type {(code: string) => ImportDeclaration | null} */
@@ -196,9 +171,10 @@ Transition.Child = ({
   return runtime;
 };
 
+/** @type {(code: string, type: BundleType) => ImportDeclaration | null} */
 const generateEmotionImportDeclaration = (code, type) => {
   const specifiers = [
-    type === "umd" ? namedImportSpecifier("jsx") : null,
+    type !== "esm" ? namedImportSpecifier("jsx") : null,
     code.includes("<Transition") ? namedImportSpecifier("ClassNames") : null,
   ].filter(Boolean);
   return specifiers.length !== 0
@@ -244,13 +220,7 @@ export const generateImports = (code, { type = "esm", preset = "clsx" } = {}) =>
     preset === "twin.macro" && generateEmotionImportDeclaration(code, type),
   ]
     .filter(Boolean)
-    .map((importDeclaration) => {
-      if (importDeclaration.source === "twin.macro") {
-        // Always generate esm imports for twin so the macro can be evaluated
-        return printImportDeclaration(importDeclaration, "esm");
-      }
-      return printImportDeclaration(importDeclaration, type);
-    })
+    .map((importDeclaration) => printImportDeclaration(importDeclaration, type))
     .join("\n");
 
   const runtime = [preset === "twin.macro" && generateTwinMacroRuntime(code)]
