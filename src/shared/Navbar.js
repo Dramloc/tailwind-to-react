e1df7b17bfc71a73a103313e8499f1f73e3fff74
Antoine Colmard <a.colmard@gmail.com>
2020-12-19 23:18:53 +0100

    ✏ Fix typo

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -2,7 +2,7 @@
 /** @jsxImportSource @emotion/react */
 import "twin.macro";
 
-/** @type {React.FC<{ start?: JSX.Element, end?: JSX.Element } & import("react").HTMLAttributes<HTMLElement>} */
+/** @type {React.FC<{ start?: JSX.Element, end?: JSX.Element } & import("react").HTMLAttributes<HTMLElement>>} */
 export const Navbar = ({ start, end, ...props }) => {
   return (
     <header tw="relative h-14 z-10 py-2 px-5 sm:px-6 flex items-center space-x-4" {...props}>
